146eb1e48319f5ca159c44f767b86767f14af58d
fenilmevada69-ai <fenilmevada69@gmail.com>
2026-05-24 16:26:21 +0530

    some message

--- final_prac.py
+++ final_prac.py
@@ -1,4 +1,36 @@
-li = [10,20,30,40,50,60,70,80]
+student = {
+    "name" : "fenil",
+    "topic" : "programming",
+    "subjects" : ["C++", "Java", "C"],    
+    "marks" : (10,20,30),
+    "isPass" : True,
+    "course" : {
+            "C++" : "royal",
+            "Java" : {
+                "night" : "royal",
+                "day" : "red & white" 
+                },
+            "C" : "Tops"
+        },
+    "isHome" : None
+}
 
-str = "string"
-print(str[2])
+new_dic = {
+    "city" : "ahmedabad",
+    "age" : 19  
+}
+
+student.update(new_dic)
+print(student["city"])
+
+
+
+
+
+d1={"phy" :90 , "che" :96, "math" : 98}
+print(len(d1))
+print(min(d1))
+print(max(d1))
+print(sorted(d1))
+# print(sum(d1))
+d1.clear()
