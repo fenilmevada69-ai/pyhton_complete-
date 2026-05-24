2fb0b6e7d9e30c0b246d3afc2066e4e600a50308
fenilmevada69-ai <fenilmevada69@gmail.com>
2026-05-25 01:52:10 +0530

    some message

--- final_prac.py
+++ final_prac.py
@@ -1,6 +1,3 @@
-d1 = {
-    "name" : "rahul",    
-    "age": 21
-}
-d1.setdefault("ss",67)    #adds key with default value (does not overwrite existing value)
-print(d1)
+age = 19
+
+ans = ("no", "yes")[age>18]
