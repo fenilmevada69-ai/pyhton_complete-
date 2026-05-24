69345b948a0aa5199360ee776a0fab0b6d7a7fc6
fenilmevada69-ai <fenilmevada69@gmail.com>
2026-05-24 12:05:25 +0530

    some chnages

--- final_prac.py
+++ final_prac.py
@@ -1,49 +1,35 @@
-str = "Hello World!"
+li1 = [1,2,3,2]
+li2 = [4,5,6]
+
+li1.extend(li2)
+
+print(li1)
+print(li2)
+
+li1.reverse()
+print(li1)
+
+print(li1)
+li1.remove(2)
+print(li1)
+
+print(li1)
+print(li2)
+li1.copy()
+print(li1)
+print(li2)
 
 
-print(str.endswith("!"))
-print(str.startswith("H"))
-print(str.capitalize())
-print(str.upper())
-print(str.lower())
-print(str.replace("Hello", "Wordl"))
-print(str.find("ello"))
-print(str.count("ello"))
-print(str.partition("ello"))
-
-    
-    
-email = "FMEADA86@GMAIL.COM"
-print(email.swapcase()) 
-
-
-name = "    iamfenilmevada    "
-print(name.title())
-print(name.strip())
-print(name.strip().isalnum())
-
-
-name = "hello World!"
-print(name.rpartition("lo"))
-
-
-text = " hel    lo"
-# print(text.rpartition("-"))
-
-
-print(text.isspace())
+li = [1,2,3,4,5,6,7]
+print(li[::-1])
+print(li[3:-5:-1])
 
 
 
-data = "javascriptaga"
-data = data.replace("a", "#", 3)
-print(data)
+a = [1,2,3]
+b = a
 
 
-text = "a,b,c"
-print(text.rsplit(","))
-
-
-name = "i am fenil mevada"
-print(name.capitalize())
-print(name.title())
+print(a,b)
+b += [1,2,3]
+print(a,b)
