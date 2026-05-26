9c1e6176164221603d9f3c6c11ee9f1a09bdf134
fenilmevada69-ai <fenilmevada69@gmail.com>
2026-05-26 15:02:42 +0530

    some message

--- final_prac.py
+++ final_prac.py
@@ -1,17 +1,7 @@
-
-def getUserData(age,salary,name):
-    print(f"name  ={name}")
-    print(f"Salary  ={salary}")
-    print(f"AGe  ={age}")
-
-
-getUserData(20,20000,"abc")    
-getUserData(23000,"pqr",23)
-
-#named param argumen....
-
-#getUserData(age=23,name="amit",salary=67000)
-#getUserData(age=23,name="amit",23000) #error
-#getUserData(age=23,name="amit",age=24) #error  keyword argument repeated: age
-#getUserData(23,name="amit",salary=34500)
-#getUserData(23,"ok",salary=34500) error...
+def users(*args):
+    print(args)
+    
+    
+users("ram", "shyam", ["ok"])
+users()
+users("ram", 22, ("ok"))
