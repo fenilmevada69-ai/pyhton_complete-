c7a423448e4d733441971ef729a911d36694e108
fenilmevada69-ai <fenilmevada69@gmail.com>
2026-05-26 11:04:07 +0530

    some message

--- final_prac.py
+++ final_prac.py
@@ -1,13 +1,3 @@
-age = 19
-
-ans = ("no", "yes")[age > 20]
-print(ans)
-
-
-
-print("Yes") if age > 18 else print("No")
-
-
-str1 = "Apna College"
-str2 = "College"
-
+str = "Hello World!"
+print(str.endswith("Hell"))
+print(str.startswith("!"))
