5ccbc29ad2d6d7fa13a7f1922f849ff001efdf02
fenilmevada69-ai <fenilmevada69@gmail.com>
2026-05-26 21:02:10 +0530

    some message

--- final_prac.py
+++ final_prac.py
@@ -1,5 +1,23 @@
-data = []
-for i in range(1,6):
-    data.append(i)
-    
+names = ["anna", "level", "radar", "madam", "civic", "noon", "racecar", "vijay", "sneha", "rohan", "alexander", "mahesh", "raj", "om", "ki"]
+
+data = ["yes" if i==i[::-1] else "no" for i in names if len(i)>4]
+data = [i for i in names if len(i)>4 and i == i[::-1]]
 print(data)
+
+
+
+data = ()
+print(type(data).__name__)
+
+
+
+words = {"hello", "by", "why", "when", "help", "solve"}
+word = {i for i in words}
+print(word)
+
+
+
+x = {i[0] for i in "abcdefghijklmnopq" if i not in "abcde"}
+print(x)
+
+
--- royal/ds/list/listCompresh2.py
+++ royal/ds/list/listCompresh2.py
@@ -36,7 +36,7 @@ units=[190,100,200,300,334,70,50,400,450,10,110]
 
 #give 20 % discount if unit us above 200 else 10% disaocunt give  after discount list
 #["171","90",160]....
-disc = [i//1.2 if i>200 else i //1.1 for i in units]
+disc = [i//1.2 if i>200 else i//1.1 for i in units]
 print(disc)
 
 
