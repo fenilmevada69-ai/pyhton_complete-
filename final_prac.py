5dde719ebff83bd4004bd26c2cc6104209a58ef4
fenilmevada69-ai <fenilmevada69@gmail.com>
2026-05-26 16:31:34 +0530

    some message

--- final_prac.py
+++ final_prac.py
@@ -1,7 +1,33 @@
-def users(*args):
-    print(args)
+def getSum(*args):
+    return sum(args)
+        
+        
+s = getSum(1,2,3,4,5)
+print(s)
+
+
+def checkDt(*args):
+    flag = True
+    for i in args:
+        if type(i) != int:
+            flag = False
+            break
+    return flag
     
     
-users("ram", "shyam", ["ok"])
-users()
-users("ram", 22, ("ok"))
+ans = checkDt("xys", 13,3,"ceh")
+print(ans)
+
+
+print("================")
+def getUserData(**kwargs):
+    print(kwargs)
+    
+    
+getUserData(name="rahul", age=21, room=2002, xender="male")
+    
+    
+    
+    
+data = "I am string"
+print(type(data) == str)    
